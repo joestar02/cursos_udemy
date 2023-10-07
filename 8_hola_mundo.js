53f97415ff63f100119e9895c5e72c1d9fbccc77
joestar02 <ramoncano@gmail.com>
2023-10-07 21:15:12 +0200

    curso

---
+++ 8_hola_mundo.js
@@ -0,0 +1,11 @@
+console.log('Hola mundo!');
+
+
+//número -> number
+console.log(34)
+console.log(34.43)
+
+//texto -> string
+console.log("pepe")
+console.log('pepe')
+console.log(`pepe`)
---
+++ 9_tipos_datos.js
@@ -0,0 +1,55 @@
+//Comentario 1 linea
+/**
+ *
+ * Comentario multilinea
+ *
+ */
+
+//número - number
+console.log(34);
+console.log(34.43);
+
+//texto - string
+console.log("pepe");
+console.log("pepe");
+console.log(`pepe`);
+
+//Logicos -booleanos
+console.log(true);
+console.log(true);
+
+//indefinido -undefined
+console.log(undefined);
+
+//null
+console.log(null);
+
+/**
+ * Tipos de datos estructurales
+ */
+//object
+console.log({
+  nombre: "pepe",
+  apellidos: "fernandez",
+  edad: 34,
+  peso: 79.4,
+  esalto: false,
+});
+
+//tipo de dato lista -array-
+console.log(["mexico", "guatemala", "paraguay", "uruguay"]);
+
+console.log(
+  [
+    23,
+    false,
+    {
+      nombre: "pepe",
+      apellidos: "fernandez",
+      edad: 34,
+      peso: 79.4,
+      esalto: false,
+    },
+  ],
+  { paises: ["mexico", "guatemala", "paraguay", "uruguay"], residencia: true }
+);
