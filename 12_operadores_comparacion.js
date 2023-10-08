15f4f4673760b41547a77050bad7cab5cb8a52f6
joestar02 <ramoncano@gmail.com>
2023-10-08 20:11:00 +0200

    operadores comparación

--- 12_operadores_comparacion.js
+++ 12_operadores_comparacion.js
@@ -1,1 +1,24 @@
-    
+/**
+ * Operadores de comparación devuelven true o false
+ */
+
+//Operador igual ==
+console.log(3==3);
+console.log(3==4);
+console.log(3=='3');
+
+//Operador no es igual !=
+console.log(3!=3);
+
+//Operador estrictamente igual ===
+console.log(3===3);
+console.log(3==='3');
+
+//Operador desigualdad estricta !==
+console.log(3!=='3');
+
+//Operador >, >=, <,<=
+console.log(3 < 4);
+console.log(3 <= 4);
+console.log(3 > 4);
+console.log(3 >= 4);
