b8b26d4afcb64ab2ba6d13abe3ef936bb11c6e21
joestar02 <ramoncano@gmail.com>
2023-10-08 19:40:15 +0200

    tema 11

---
+++ 11_operadores_asignacion.js
@@ -0,0 +1,48 @@
+/**
+ * Operadores
+ */
+
+
+/**
+ * Operadores de asignación
+ */
+
+//Operador de asignación
+var pepe = 'pepe';
+
+//Operador de asignación de adición +=
+var x = 2;
+var y = 1;
+x +=y;
+
+console.log(x);
+
+//Operador de asignación de resta -=
+var x = 2;
+var y = 1;
+x -=y;
+console.log(x);
+
+//Operador de asignación de multiplicación +=
+var x = 2;
+var y = 3;
+x *=y;
+console.log(x);
+
+//Operador de asignación de division /=
+var x = 1;
+var y = 2;
+x /=y;
+console.log(x);
+
+//Operador de asignación de modulo o residuo %=
+var x = 1;
+var y = 2;
+x %=y;
+console.log(x);
+
+//Operador de asignación de exponente **=
+var x = 2;
+var y = 3;
+x**=y;
+console.log(x);
